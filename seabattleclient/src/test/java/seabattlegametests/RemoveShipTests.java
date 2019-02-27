6b90ea808dacaccd0797318df22440dd04ad31fe
Mario Lucassen <mario.lucassen@hotmail.com>
2019-02-27 14:53:32 +0100

    Added tests for removing ships

--- seabattleclient/src/test/java/seabattlegametests/RemoveShipTests.java
+++ seabattleclient/src/test/java/seabattlegametests/RemoveShipTests.java
@@ -1,6 +1,9 @@
 package seabattlegametests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import seabattlegui.ShipType;
+import seabattlegui.SquareState;
 import seabattleunittests.SeaBattleGameTests;
 
 /**
@@ -12,8 +15,31 @@ import seabattleunittests.SeaBattleGameTests;
  * param posY     y-coordinate of square where ship was placed
  */
 public class RemoveShipTests extends SeaBattleGameTests {
-    @Test
-    public void should_Remove_Ship_On_1X_1Y_Successfully() {
+    // TODO: ask Nico about what happens when a coordinate is given that is out of range or is at a location that does not have a ship on it.
 
+    @Test
+    public void should_Remove_Ship_On_1X_1Y_When_Previously_Placed_On_1X_1Y() {
+        // Arrange
+        game.placeShip(1, ShipType.AIRCRAFTCARRIER, 1, 1,true);
+
+        // Act
+        game.removeShip(1,1,1);
+
+        // Assert
+        SquareState playerSquareState = applicationPlayer.getPlayerSquareState(1,1);
+        Assertions.assertEquals(SquareState.WATER, playerSquareState);
+    }
+
+    @Test
+    public void should_Remove_AIRCRAFTCARRIER_On_5X_1Y_When_Previously_Placed_On_1X_1Y() {
+        // Arrange
+        game.placeShip(1, ShipType.AIRCRAFTCARRIER, 1, 1,true);
+
+        // Act
+        game.removeShip(1,5,1);
+
+        // Assert
+        SquareState playerSquareState = applicationPlayer.getPlayerSquareState(1,1);
+        Assertions.assertEquals(SquareState.WATER, playerSquareState);
     }
 }
