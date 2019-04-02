d29db9fa4232a456eb2d3298ff89948ec91d41ba
Raphael <r.vanakkerveken@student.fontys.nl>
2019-04-02 15:19:45 +0200

    Fixed dependency issues and rmoved HttpCommunicator

--- seabattleserver/src/main/java/module-info.java
+++ seabattleserver/src/main/java/module-info.java
@@ -1,7 +1,4 @@
 module seabattleserver {
     requires socketmessaging;
     requires seabattledomain;
-    requires org.apache.httpcomponents.httpcore.nio;
-    requires org.apache.httpcomponents.httpcore;
-
 }
