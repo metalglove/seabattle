e68dd4d6b364ad31629e63da23af42a372e3e616
Raphael <r.vanakkerveken@student.fontys.nl>
2019-04-05 17:04:04 +0200

    Added register test.

--- seabattleserver/src/test/java/seabattleservertests/RegisterPlayerTest.java
+++ seabattleserver/src/test/java/seabattleservertests/RegisterPlayerTest.java
@@ -12,8 +12,7 @@ import services.SeaBattleGameService;
 import utilities.ShipCreationArgument;
 import utilities.ShipFactory;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RegisterPlayerTest {
     private ISeaBattleGameService seaBattleGameService;
@@ -37,5 +36,19 @@ public class RegisterPlayerTest {
         // Assert
         assertTrue(registerPlayerResultDto.isSuccess());
         assertEquals(0, (int)registerPlayerResultDto.getOpponentPlayerNumber(), "The opponents ID is higher than 0 and thus is not an AI which rules out it being a SinglePlayer game.");
+    }
+
+    @Test
+    public void should_Throw_Null_Pointer_Error_When_Registering_Player_Henk_With_Password_Karel32_To_A_Multiplayer_Game_When_MultiPlayer_Is_True_And_No_Opponent_Registers() {
+        // Arrange
+        Player player = new Player("Henk", "Karel32", 1);
+        boolean multiPlayer = true;
+
+        // Act
+        RegisterPlayerResultDto registerPlayerResultDto = seaBattleGameService.registerPlayer(player, multiPlayer);
+
+        // Assert
+        assertTrue(registerPlayerResultDto.isSuccess());
+        assertNull(registerPlayerResultDto.getOpponentPlayerNumber(), "The opponents ID is not null, which means AI got registered and thus is a SinglePlayer game.");
     }
 }
